04eb63ba2cfecbbb86d9eff7a9d4abb43f5acef2
BFaustino7 <faustibarvona17@gmail.com>
2024-10-29 13:42:30 -0300

    HW6-complete

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,15 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+ 
+  var producto = 1;
+  if(a <= 0){
+    return 0
+  }
+  for(var i = a; i <= b; i++){
+    producto *= i;
+  }
+  return producto
 }
 
 module.exports = productoEntreNúmeros;
--- C6 Bucles/ejercicios/21.js
+++ C6 Bucles/ejercicios/21.js
@@ -4,6 +4,27 @@ function esPotenciaDeDos(numero) {
   // Devuelve true si lo es, sino devuelve false.
   // PISTA: Utiliza un bucle while.
   // Tu código:
+  
+ // var n = Math.log2(numero);
+ // var raizN = Math.pow(numero, 1 / n);
+ // while(n >= 1){
+ //   if(raizN !== 2){
+ //     return false
+ // } else if(raizN === 2)
+ //   return true
+ // }
+  
+ 
+ var i = numero
+ while(i >= 1){
+   if(i === 1){
+     return true
+   }
+    i = i / 2
+ }
+  return false
 }
 
+
 module.exports = esPotenciaDeDos;
+
